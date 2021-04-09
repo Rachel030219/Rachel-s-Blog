e8505873ca76427c4191b0456978650bab30e3b6
Rachel030219 <tangrui003@gmail.com>
2021-04-10 00:28:59 +0800

    added dark mode and modified theme

--- themes/toki/source/js/toki.js
+++ themes/toki/source/js/toki.js
@@ -4,9 +4,7 @@ $(document).ready(setTimeout(() => {
     var items = $('.menu-item-link')
         items.each(function(index,element){
             console.log($(this).attr("href"))
-            if($(this).attr("href") == url) {$(this).css('color','#607d8b')}else{
-                $(this).css('color','#333')
-            }
+            if($(this).attr("href") == url) {$(this).css('color','#607d8b')}
       });
     
     var search = document.querySelector('.search')
